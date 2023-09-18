3ff0eb87e0a776d53f180e9d7597a154fec7a103
Henrique Chaves <henriquechavesmm@gmail.com>
2023-09-18 15:27:17 +0200

    Add security to /service/** endpoints

--- src/main/java/com/imt/part/configuration/SecurityConfiguration.java
+++ src/main/java/com/imt/part/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.imt.part.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,6 +14,9 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.GET, "/service/**").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/service/**").hasRole("admin")
+                .antMatchers("/service/**").hasRole("user")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
